85a83333290d963e5e2a10f1536aac8cd3398396
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-01-30 14:31:02 +0100

    Update

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export default function Nav({category}) {
+    const categoryList = ["City", "Ninjago", "Castles and Knights", "Marine and Pirates", "Movie Characters"]
+
+    return (
+    <nav>
+            <ul>
+                {categoryList.map((item, index) => <li key={index+"Cat"}><a className={category === item ? "active" : null} href="#">{item}</a></li>)}
+            </ul>
+        </nav>
+)
+
+}
