21ff8cdd544ad881f5f02bdf99dc4da5391baf26
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-06 15:03:32 +0100

    lagt til linker med routing til kategori komponentet slik at man kan

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
+<Router>
     <App />
-  </Router>,
+</Router>,
 )
