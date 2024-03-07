f0099fdce7b4571286b3b3aff2a2ab98c475f738
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-07 22:08:55 +0100

    oppsett Food API

---
+++ Food_api/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [food, setFood]= useState("")
+
+  const getFood = async() =>{
+    .fetch {}
+    .then {}
+    .then{}
+    .then{}
+
+
+    
+  }
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
