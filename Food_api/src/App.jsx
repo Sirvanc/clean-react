ae8d5fb840d2b95f92cde47de4c93d22d94530ac
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-07 22:15:52 +0100

    Oppdatert

--- Food_api/src/App.jsx
+++ Food_api/src/App.jsx
@@ -2,23 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [food, setFood]= useState("")
 
-  const getFood = async() =>{
-    .fetch {}
-    .then {}
-    .then{}
-    .then{}
+  const [food, setFood] = useState("")
+  const apiKey = process.env.REACT_APP_API_KEY
 
-
-    
+  const getFood = async()=>{
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&cuisine=greek`)
+    .then(response => response.json())
+    .then(data => setFood(data.results))
+    .catch(error => console.error(error))
   }
+
+  useEffect(()=>{
+    getFood()
+  },[])
+
+  console.log(food)
 
   return (
     <>
-      
+      <h1>Hallo World!</h1>
     </>
   )
 }
